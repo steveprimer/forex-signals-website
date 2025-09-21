75434dbfad70cfdf334c78e5994bf12eb6ad44b8
Jack Rider <jack666rider@gmail.com>
2025-09-22 02:51:44 +0530

    commit both branches

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import VideoSection from "@/components/VideoSection"; // 1. Import the new component
+import StatsSection from "@/components/StatsSection";
+import Services from "@/components/Services";
 
 export default function HomePage() {
   return (
     <main className="bg-black min-h-screen text-white">
       <Navbar />
       <Hero />
+      <StatsSection />
+      <VideoSection />
+      <Services />
       {/* You can add the rest of your page content here */}
     </main>
   );
